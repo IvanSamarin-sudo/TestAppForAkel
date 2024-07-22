6847a010c029a1e48e7f7854c918b0b1abe826c2
IvanSamarin-sudo <sir.ballock@gmail.com>
2024-07-22 22:52:57 +0300

    Add files via upload
    Загрузка проекта в репозиторий.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+
+namespace AkelonTestv1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Worker> workers = new List<Worker>();
+
+            Vacation vacation = new Vacation();
+
+            List<string> names = new List<string>()
+            {
+                "Иванов Иван Иванович",
+                "Петров Петр Петрович",
+                "Юлина Юлия Юлиановна",
+                "Сидоров Сидор Сидорович",
+                "Павлов Павел Павлович",
+                "Георгиев Георг Георгиевич"
+            };
+
+            foreach (var name in names)
+            {
+                workers.Add(new Worker(name));
+            }
+
+            foreach (var worker in workers)
+            {
+                try
+                {
+                    do
+                    {
+                        worker.Vacations.Add(vacation.GetVacation(worker));
+                    }
+                    while (worker.DaysOfVacation > worker.DaysDistributed);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            foreach (var worker in workers)
+            {
+                Console.WriteLine(worker.ToString());
+            }
+        }
+    }
+
+}
---
+++ Worker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkelonTestv1
+{
+    /// <summary>
+    /// Работник.
+    /// </summary>
+    [Serializable]
+    public class Worker
+    {
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Список отпусков сотрудника.
+        /// </summary>
+        public List<List<DateTime>> Vacations;
+
+        /// <summary>
+        /// Имеет ли сотрудник дополнительные дни отпуска. (В связи с вредными условиями труда и/или ненормированным рабочим графиком).
+        /// </summary>
+        public bool HaveExtraDays = false;
+
+        /// <summary>
+        /// Допустимое количество дней отпуска.
+        /// </summary>
+        public int DaysOfVacation { get; private set; }
+
+        /// <summary>
+        /// Уже распределённые дни отпуска.
+        /// </summary>
+        public int DaysDistributed = 0;
+
+        /// <summary>
+        /// Назначить отпуск сотруднику.
+        /// </summary>
+        /// <param name="dates"></param>
+        public void SetVacation(List<DateTime> dates)
+        {
+            Vacations.Add(dates);
+        }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="vacations"></param>
+        public Worker(string name)
+        {
+            Name = name;
+            Vacations = new List<List<DateTime>>();
+            DaysOfVacation = HaveExtraDays ? 31 : 28;
+        }
+
+        /// <summary>
+        /// Преобразовать список списков дат в строку.
+        /// </summary>
+        /// <returns></returns>
+        string VacationsToString()
+        {
+            string res = String.Empty;
+
+            foreach (var vac in Vacations)
+            {
+                var firstDate = vac[0];
+                var lastDate = vac[vac.Count - 1];
+                    res += "\r\n" + firstDate.ToString() + "-" + lastDate.ToString();
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Преобразовать класс в строку.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        { 
+            return Name + VacationsToString();
+        }
+
+    }
+}
